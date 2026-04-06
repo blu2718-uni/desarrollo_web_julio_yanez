4da0f46433784e65a2d1df3b5de49ba8812fdca5
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-04-06 12:31:00 -0400

    feat: added basic content to index.html, with the capacity of redirecting to every other page

---
+++ script.js
@@ -0,0 +1,8 @@
+const a_registro = document.getElementById("a-registro")
+a_registro.addEventListener("click", () => {window.location.href = "./registro.html"})
+
+const a_entradas = document.getElementById("a-entradas")
+a_registro.addEventListener("click", () => {window.location.href = "./entradas.html"})
+
+const a_metricas = document.getElementById("a-metricas")
+a_registro.addEventListener("click", () => {window.location.href = "./metricas.html"})
