678af3f62bf1640eac9ba9dff78c3fdf232a89e0
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-04-12 18:00:24 -0400

    refactor: added further input sanitization

--- js/re_registro.js
+++ js/re_registro.js
@@ -1,8 +1,8 @@
 const reRegistrar = () => {
     const params = new URLSearchParams(window.location.search);
-    const nombre = params.get("nombre");
-    const email = params.get("email");
-    const rol = params.get("rol");
+    const nombre = encodeURIComponent(params.get("nombre"));
+    const email = encodeURIComponent(params.get("email"));
+    const rol = encodeURIComponent(params.get("rol"));
 
     window.location.href = `./registro.html?nombre=${nombre}&email=${email}&rol=${rol}`
 }
