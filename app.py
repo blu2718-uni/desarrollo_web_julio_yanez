23663a2ad6701d6628af80427e3cd9e4f7a2c5e5
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-05-06 18:11:05 -0400

    chore: made ultra basic flask app

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template, redirect, url_for, session
+from database import db
+from werkzeug.utils import secure_filename
+import hashlib
+import filetype
+import os
+import uuid
+
+UPLOAD_FOLDER = 'static/uploads'
+
+app = Flask(__name__)
+app.secret_key = "secret_key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0" ,debug=True)
