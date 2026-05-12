6d7c2552f4999e404446abec1e86cf3f3356a1f4
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-05-12 12:11:18 -0400

    feat: fixed redirection and constructed templates for the existing proyect

--- app.py
+++ app.py
@@ -13,9 +13,24 @@ app.secret_key = "secret_key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html", a_index=False)
+
+@app.route("/registro", methods=["GET", "POST"])
+def registro():
+    if request.method == "GET":
+        return render_template("registro.html", a_index=True)
+
+@app.route("/entradas", methods=["GET", "POST"])
+def entradas():
+    if request.method == "GET":
+        return render_template("entradas.html", a_index=True)
+
+@app.route("/metricas", methods=["GET", "POST"])
+def metricas():
+    if request.method == "GET":
+        return render_template("metricas.html", a_index=True)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0" ,debug=True)
