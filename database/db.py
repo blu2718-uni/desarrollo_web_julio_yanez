44ee0e4ccf5a730d4c03cb74ab097b04367f6637
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-05-12 17:22:08 -0400

    feat: implemented classes in the database

--- database/db.py
+++ database/db.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, BigInteger, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime, Enum, Text
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
-import json
+from datetime import datetime
 
 DB_NAME = "tarea2"
 DB_USERNAME = "cc5002"
@@ -14,3 +14,55 @@ engine = create_engine(DATABASE_URL, echo=False, future=True)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
+
+class Region(Base):
+    __tablename__ = 'region'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(200), nullable=False)
+
+    comunas = relationship("Comuna", back_populates="region")
+
+class Comuna(Base):
+    __tablename__ = 'comuna'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(200), nullable=False)
+    region_id = Column(Integer, ForeignKey('region.id'), nullable=False)
+
+    region = relationship("Region", back_populates="comunas")
+    miembros = relationship("Miembro", back_populates="comuna")
+
+class Miembro(Base):
+    __tablename__ = 'miembro'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String(255), nullable=False)
+    email = Column(String(80), nullable=False)
+    telefono = Column(String(15), nullable=False)
+    rol = Column(String(45), nullable=False)
+    fecha_registro = Column(DateTime, nullable=False, default=datetime.now)
+    comuna_id = Column(Integer, ForeignKey('comuna.id'), nullable=False)
+
+    comuna = relationship("Comuna", back_populates="miembros")
+    actividades = relationship("Actividad", back_populates="miembro")
+
+class Actividad(Base):
+    __tablename__ = 'actividad'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    miembro_id = Column(Integer, ForeignKey('miembro.id'), nullable=False)
+    dia = Column(Enum('lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'), nullable=False)
+    hora_inicio = Column(String(5), nullable=False)
+    duracion = Column(String(5), nullable=False)
+    tipo = Column(Enum('arte', 'deporte', 'tecnología', 'social', 'recreación', 'otra'), nullable=False)
+    nombre = Column(String(45), nullable=False)
+    descripcion = Column(Text(500), nullable=True)
+
+    miembro = relationship("Miembro", back_populates="actividades")
+    fotos = relationship("Foto", back_populates="actividad")
+
+class Foto(Base):
+    __tablename__ = 'foto'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ruta_archivo = Column(String(300), nullable=False)
+    nombre_archivo = Column(String(300), nullable=False)
+    actividad_id = Column(Integer, ForeignKey('actividad.id'), nullable=False)
+
+    actividad = relationship("Actividad", back_populates="fotos")
