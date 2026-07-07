6c201fa853599cfa4367945dd5a5d49529817e98
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-07-07 10:15:47 -0400

    fix: removed unused lines

--- tarea4/src/main/java/com/tarea4/tarea4/controllers/AppController.java
+++ tarea4/src/main/java/com/tarea4/tarea4/controllers/AppController.java
@@ -3,16 +3,8 @@ package com.tarea4.tarea4.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.tarea4.tarea4.services.BuscadorService;
-
 @Controller
 public class AppController {
-
-    private final BuscadorService buscadorService;
-
-    public AppController(BuscadorService buscadorService) {
-        this.buscadorService = buscadorService;
-    }
 
     @GetMapping("/")
     public String index() {
